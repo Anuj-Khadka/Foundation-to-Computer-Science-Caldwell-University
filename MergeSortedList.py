0d8306757ee20f1588c87004dd15ffd8ac869d91
Anuj Khadka <anujkhadka008@gmail.com>
2023-08-10 22:51:14 +0545

    Create MergeSortedList.py

---
+++ MergeSortedList.py
@@ -0,0 +1,16 @@
+l1 = [11, 12, 14, 20]
+l2 = [4, 5, 12, 21]
+l3 = []
+
+# for i in range(len(l2)):
+#   tv = l2[0]
+#   l2.remove(tv)
+#   l1.append(tv)
+
+# print(l1)
+i = 0
+
+while i < len(l1) and j < len(l2):
+  if 
+  
+  

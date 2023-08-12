c47339243580dd9a5ba7b6fd0e32011e1cdc7d1e
Anuj Khadka <anujkhadka008@gmail.com>
2023-08-12 22:04:49 +0545

    Update TwoSums.py

--- TwoSums.py
+++ TwoSums.py
@@ -1,7 +1,7 @@
 list = [3, 12, 8, 13, 2, 5, 7]
 target = 10
 
-for i in range(len(list) - ):
+for i in range(len(list) - 1):
   for j in range(i+1):
     if list[i] + list[j] == target:
       return i,j
